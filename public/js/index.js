a799aa52031bcc0c5d8cb591482039e89d50e6b7
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2022-05-19 21:15:47 -0500

    Base de index.js completa

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,17 @@
 
-//console.log("Funciona!");
-
 // 
 function showWelcome(){
   document.getElementById("main").innerHTML = "<h1>¡Bienvenido!<br>Usted puede escribir archivos Markdown en Menú/Crear Documento y revisar sus creaciones en Menú/Lista</h1>"; // evento
 }
 
 function showListMarkdown(){
-  
+  document.getElementById("main").innerHTML = "Página de lista archivos Markdown"; // evento
 }
 
 function showCreateDocument(){
-  
+  document.getElementById("main").innerHTML = "Página de creación archivo Markdown"; // evento
 }
+
+
+console.log("Funciona!");
+
